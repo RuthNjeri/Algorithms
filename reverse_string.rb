e041c37afb78601ddad2ead78f54ef762c9f034a
RuthNjeri <ruthnwaiganjo@gmail.com>
2020-02-11 02:55:06 +0300

    reverse_string

---
+++ reverse_string.rb
@@ -0,0 +1,20 @@
+# @param {Character[]} s
+# @return {Void} Do not return anything, modify s in-place instead.
+def reverse_string(s)
+    len = s.size
+    last_counter = 1
+    first_counter = 0
+
+    for letter in s
+        temp = s[len - last_counter]
+        s[len - last_counter] = letter
+        s[first_counter] = temp
+        last_counter += 1
+        first_counter += 1
+        if first_counter == (s.size/2)
+            return s
+        end
+    end
+end
+
+puts reverse_string(["h","e","l"])
